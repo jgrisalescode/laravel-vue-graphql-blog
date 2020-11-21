88ba6de4cb1e60f6b536ec3ad622496fb925bc40
JuliaGrisales <jgrisales@gmail.com>
2020-11-21 21:16:38 +0100

    Setting Up Tailwind CSS & Vue.js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,96 +3,24 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <title>BlogQL</title>
+        <link rel="stylesheet" href="{{mix('css/app.css')}}">
+        <script src="{{mix('js/app.js')}}" defer></script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <div id="app">
+            <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+                <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src="https://randomuser.me/api/portraits/women/79.jpg" alt="Woman's Face">
+                <div class="text-center space-y-2 sm:text-left">
+                    <div class="space-y-0.5">
+                    <p class="text-lg text-black font-semibold">
+                        Erin Lindford
+                    </p>
+                    <p class="text-gray-500 font-medium">
+                        Product Engineer
+                    </p>
+                    </div>
+                    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
                 </div>
             </div>
         </div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -16,3 +16,7 @@ mix.postCss('resources/css/app.css', 'public/css', [
 ])
 
 mix.js('resources/js/app.js', 'public/js')
+
+if (mix.inProduction()) {
+    mix.version()
+}
