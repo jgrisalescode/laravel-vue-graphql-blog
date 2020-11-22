b5a703a6084efd8564f2a81bbbccf0f14b0d1d88
JuliaGrisales <jgrisales@gmail.com>
2020-11-22 21:34:30 +0100

    Setting Up Routes in Vue Router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,30 @@
-import './bootstrap';
 import Vue from 'vue';
 import VueRouter from 'vue-router'
+import './bootstrap';
+import PostList from './PostList'
+import Post from './Post'
 
+window.Vue = Vue;
 Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/',
+        name: 'index',
+        component: PostList
+    },
+    {
+        path: '/post/:id',
+        name: 'post',
+        component: Post
+    }
+]
+
+const router = new VueRouter({
+    routes
+})
 
 const app = new Vue({
     el: '#app',
+    router
 });
